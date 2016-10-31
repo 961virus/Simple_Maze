594674536209d3c6ae4cfa01a2fc4a3a7e3251c5
virus <evilvirus@nibox.ru>
2016-10-31 16:40:01 +0400

    add buttons

--- app/src/main/java/com/example/virus/simplemaze/MainActivity.java
+++ app/src/main/java/com/example/virus/simplemaze/MainActivity.java
@@ -3,17 +3,62 @@ package com.example.virus.simplemaze;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
 
-
+    Button Left;
+    Button Up;
+    Button Right;
+    Button Down;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Left = (Button) findViewById(R.id.button_left);
+        Up = (Button) findViewById(R.id.button_up);
+        Right = (Button) findViewById(R.id.button_right);
+        Down = (Button) findViewById(R.id.button_down);
+
+//creating click listener for buttons
+        View.OnClickListener oclLeft = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        };
+
+        View.OnClickListener oclUp = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        };
+
+        View.OnClickListener oclRight = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        };
+
+        View.OnClickListener oclDown = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        };
+
+        //set click listeners
+        Left.setOnClickListener(oclLeft);
+        Up.setOnClickListener(oclUp);
+        Right.setOnClickListener(oclRight);
+        Down.setOnClickListener(oclDown);
     }
 
 
